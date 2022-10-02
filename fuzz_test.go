287618830aeb6f250c5ac9e50aa921f2a7976834
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-10-02 05:51:38 +0000

    Update Tests
    Signed-off-by: Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>

---
+++ fuzz_test.go
@@ -0,0 +1,30 @@
+package fuzztestingingo
+
+import "testing"
+
+func TestEqual(t *testing.T) {
+	type args struct {
+		a []byte
+		b []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Equal(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Equal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func FuzzEqual(f *testing.F) {
+	f.Fuzz(func(t *testing.T, a []byte, b []byte) {
+		Equal(a, b)
+	})
+}
